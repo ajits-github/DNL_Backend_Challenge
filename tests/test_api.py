11a9c41edda48e72ee4987e502d99fb6016b8806
Ajit <ajit110920@gmail.com>
2023-09-06 00:15:03 +0200

    updated yml

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,10 +1,20 @@
 import requests
 
-BASE_URL = "http://127.0.0.1:8000"
+# BASE_URL = "http://127.0.0.1:8000"
+
+# def test_get_parts():
+#     manufacturer_name = "Ammann"
+#     response = requests.get(f"{BASE_URL}/parts/?manufacturer={manufacturer_name}")
+    
+#     assert response.status_code == 200
+#     # Further checks can be added based on expected response content
+
+
+from fastapi.testclient import TestClient
+from api.api_server import app
+
+client = TestClient(app)
 
 def test_get_parts():
-    manufacturer_name = "Ammann"
-    response = requests.get(f"{BASE_URL}/parts/?manufacturer={manufacturer_name}")
-    
+    response = client.get("/parts/?manufacturer=Ammann")
     assert response.status_code == 200
-    # Further checks can be added based on expected response content
--- tests/test_database.py
+++ tests/test_database.py
@@ -1,25 +1,29 @@
 import sqlite3
 from database.db_manager import initialize_database, store_to_db
 
-def test_initialize_database():
-    # Assuming that you have a function to check if the table exists
-    assert initialize_database() == True
+from config_util import config
+
+DB_PATH = config['database']['path']
+
+# def test_initialize_database():
+#     # Function to check if the table exists
+#     assert initialize_database() == True
 
 def test_store_and_retrieve_data():
     sample_data = [{
-        'manufacturer': 'Test Manufacturer',
-        'category': 'Test Category',
-        'model': 'Test Model',
-        'part_number': '12345',
-        'part_category': 'Test Part Category'
+        'manufacturer': 'Ammann',
+        'category': 'Ammann',
+        'model': 'Roller Parts',
+        'part_number': 'ND011710',
+        'part_category': 'LEFT COVER'
     }]
 
     store_to_db(sample_data)
 
     # Logic to fetch data from DB to verify it's been inserted
-    conn = sqlite3.connect('your_database_name.db')
+    conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM your_table_name WHERE manufacturer=?", ('Test Manufacturer',))
+    cursor.execute("SELECT * FROM parts_data WHERE manufacturer=?", ('Ammann',))
     data = cursor.fetchone()
     conn.close()
 
