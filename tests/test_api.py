98d5704f5d374c510732f96a070d3995ab3a4d4a
Ajit <ajit110920@gmail.com>
2023-09-06 00:35:48 +0200

    changed yml

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,5 +1,6 @@
-import requests
 
+# ======================================================
+# import requests
 # BASE_URL = "http://127.0.0.1:8000"
 
 # def test_get_parts():
@@ -9,13 +10,23 @@ import requests
 #     assert response.status_code == 200
 #     # Further checks can be added based on expected response content
 
+# ======================================================
+# from fastapi.testclient import TestClient
+# from api.api_server import app
 
-from fastapi.testclient import TestClient
-from api.api_server import app
+# client = TestClient(app)
 
-client = TestClient(app)
+# def test_get_parts():
+#     # response = client.get("/parts/?manufacturer=Ammann")
+#     response = client.get("/parts/")
+#     assert response.status_code == 200
 
-def test_get_parts():
-    # response = client.get("/parts/?manufacturer=Ammann")
-    response = client.get("/parts/")
-    assert response.status_code == 200
+
+# ======================================================
+import requests
+
+BASE_URL = "http://127.0.0.1:8000"
+
+def test_access_website():
+    response = requests.get(BASE_URL)
+    assert response.status_code == 200, f"Expected status code 200, but got {response.status_code}"
